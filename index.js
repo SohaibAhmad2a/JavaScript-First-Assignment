9bc906154987020880da88a2601b4b9b3f92c588
Sohaib Ahmad <sohaibahmad2a@gmail.com>
2022-06-04 06:25:03 -0700

    Add Index.js

---
+++ index.js
@@ -0,0 +1,39 @@
+/*alert("JavaScript Alert\n"+"\nWelcome to JS Land...\nHappy Coding!");
+var firstName;
+var lastName;
+var age;
+var profession;
+var email="universe@gmail.com";
+firstName=window.prompt("Enter your First Name: ");
+lastName=window.prompt("Enter your last: ");
+age=prompt("Please enter your age: ");
+profession=prompt("What is your profession");
+alert(firstName+"  "+lastName);
+alert("You are: "+ age + "years old")
+alert("Your profession is: "+profession)
+alert("my email address is: "+email)
+document.write("Yeh! I can write HTML content through JavaScript")
+var myAge=29;
+alert("I am "+myAge+" years old")
+var birthYear =1992;
+document.write("My birth year is "+birthYear)
+var x*/
+//Why <br> is working and not\n?
+/*document.write("Variable names can only contain number, $, and _. For Example: $my_1stVariable<br>")
+document.write("Variable must begin with a letter, $, or _.For example $name, _name or name<br>")
+document.write("Variable names are case sensitive<br>")
+document.write("Variable names should not be JS keyword")*/
+//How can a program get integar or float values as input?
+/*let firstNumber=prompt("Please Enter your first Number= ");
+let secondNumber=prompt("Please Enter your second Number= ");
+var first =parseFloat(firstNumber);
+var second =parseFloat(secondNumber);
+var thirdNumber=first+second;
+document.write(thirdNumber)*/
+//Q#10
+var x
+/*document.write("Value after variable declaration is: "+x+".<br>")
+x=2
+document.write("Initial value: "+x+".<br>")
+++x
+document.write("Value after increment is: "+x+".")*/
