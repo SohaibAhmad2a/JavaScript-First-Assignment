561f659c7e31c34a559da8dda300bbbca774a8d5
Sohaib Ahmad <sohaibahmad2a@gmail.com>
2022-06-05 03:25:56 -0700

    Committed after Adding Question Number for each answer

--- index.js
+++ index.js
@@ -1,42 +1,47 @@
-/*alert("JavaScript Alert\n"+"\nWelcome to JS Land...\nHappy Coding!");
+//Q#1
+alert("JavaScript Alert\n"+"\nWelcome to JS Land...\nHappy Coding!");
+//Q#2
 var firstName;
 var lastName;
 var age;
 var profession;
-var email="universe@gmail.com";
-firstName=window.prompt("Enter your First Name: ");
-lastName=window.prompt("Enter your last: ");
+firstName=prompt("Enter your First Name: ");
+lastName=prompt("Enter your last: ");
 age=prompt("Please enter your age: ");
 profession=prompt("What is your profession");
 alert(firstName+"  "+lastName);
 alert("You are: "+ age + "years old")
 alert("Your profession is: "+profession)
+//Q#3
+var email="universe@gmail.com";
 alert("my email address is: "+email)
+//Q#4
 document.write("Yeh! I can write HTML content through JavaScript")
+//Q#5
 var myAge=29;
 alert("I am "+myAge+" years old")
+//Q#6
 var birthYear =1992;
 document.write("My birth year is "+birthYear)
-var x*/
-//Why <br> is working and not\n?
-/*document.write("Variable names can only contain number, $, and _. For Example: $my_1stVariable<br>")
+//Q#7
+document.write("Variable names can only contain number, $, and _. For Example: $my_1stVariable<br>") //Why <br> is working and not\n?
 document.write("Variable must begin with a letter, $, or _.For example $name, _name or name<br>")
 document.write("Variable names are case sensitive<br>")
-document.write("Variable names should not be JS keyword")*/
-//How can a program get integar or float values as input?
-/*let firstNumber=prompt("Please Enter your first Number= ");
+document.write("Variable names should not be JS keyword")
+//Q#8
+let firstNumber=prompt("Please Enter your first Number= ");//How can a program get integar or float values as input?
 let secondNumber=prompt("Please Enter your second Number= ");
 var first =parseFloat(firstNumber);
 var second =parseFloat(secondNumber);
 var thirdNumber=first+second;
-document.write(thirdNumber)*/
+document.write(thirdNumber)
 //Q#10
 var x
-/*document.write("Value after variable declaration is: "+x+".<br>")
+document.write("Value after variable declaration is: "+x+".<br>")
 x=2
 document.write("Initial value: "+x+".<br>")
 ++x
-document.write("Value after increment is: "+x+".")*/
+document.write("Value after increment is: "+x+".")
 //Q#11, First Solution:
 var celsiusTemperture=39;
 var fahrenheitTemperature;
